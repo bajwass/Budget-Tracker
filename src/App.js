9df2689dec04bb2f66c1d63ba8b39ac9da2ed4d4
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-12 21:19:57 -0700

    Different charts are now showing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details';
 
 const App = () => {
   return (
     <div>
-        hello
+        <Grid container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+            <Grid item xs={12} sm={4}>
+                 <Details />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                 Main
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                 <Details />
+            </Grid>
+
+        </Grid>
 
     </div>
   )
