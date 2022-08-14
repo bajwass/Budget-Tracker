19fcf2b05fa94f0d37f67d5142120d0378c52ad1
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-14 16:56:33 -0700

    Import context into the list

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
 
   return (
     <Card className={classes.root}>
-        <CardHeader title="Expense Tracker" subheader="Powered by Speechly"/>
+        <CardHeader title="Budget Tracker" subheader="Powered by Speechly"/>
         <CardContent>
             <Typography align="center" variant="h5">Total Balance $100</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
