f6c7591ead2d7afd1c13d78f045f9529d79bc7e5
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-14 18:01:35 -0700

    linked the context and contextreducers

--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,17 @@
 import React, { useReducer, createContext } from 'react';
+import contextReducer from './contextReducer';
 
 const initialState =[];
 
 export const BudgetTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+    const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+    //Action Creators
+
     return (
-        <BudgetTrackerContext.Provider value={{ aappName: 'Budget Tracker'}}>
+        <BudgetTrackerContext.Provider value={{ appName: 'Budget Tracker'}}>
             {children}
         </BudgetTrackerContext.Provider>
     )
