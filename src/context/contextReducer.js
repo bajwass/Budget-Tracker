29dc31b344012f40a42db59ecbf8650e1cf2afcf
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-15 19:33:34 -0700

    Update contextReducer.js
    ContextReducer initialized

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,19 +1,17 @@
-const transactions = [
-    { id: 1, },
-    { id: 2,}
-]
-
 const contextReducer = (state, action) => {
+    let transactions;
+
     switch(action.type){
         case 'DELETE_TRANSACTION':
-            const transactions = state.filter((t) => t.id !== action.payload);
-            break;
+            transactions = state.filter((t) => t.id !== action.payload);
+            return transactions;
         
         case 'ADD_TRANSACTION':
-            break;
+            transactions =[action.payload, ...state];
+            return transactions;
         
         default:
-            break;
+            return state;
 
     }
 }
