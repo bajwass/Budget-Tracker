d7fbe04e826752fb2d14a3224fac72560dd80950
Sumeet <sumeet-bajwa-34@live.ca>
2022-08-16 21:10:11 -0700

    Info Card added

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const InfoCard = () => {
+  return (
+    <div elevation={3} style={{ textAlign: 'center', padding: '0 10%' }}>
+      Add an Expense or Income below:
+    
+    </div>
+  );
+};
+
+export default InfoCard;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import { BudgetTrackerContext } from '../../context/context';
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
+import InfoCard from '../InfoCard';
 
 const Main = () => {
     const classes = useStyles();
@@ -16,8 +17,7 @@ const Main = () => {
         <CardContent>
             <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-                {/* <InfoCard /> */}
-                Try Saying: Add Income for $100 in Category Salary for Monday...
+                <InfoCard />
             </Typography>
             <Divider />
             <Form />
